241a3a968ca080c5378a946f335c4de5c70d3557
Sabrina Otoni da Silva <sabrina.silva1@ibm.com>
2024-02-20 23:25:17 -0300

    bayesian optimization added

--- scripts/cnn_trading_classifier.py
+++ scripts/cnn_trading_classifier.py
@@ -1,14 +1,18 @@
 from keras.models import Sequential
 from keras.layers import Conv1D, BatchNormalization, MaxPooling1D, LSTM, Bidirectional, Dropout, Dense
+from keras.optimizers import Adam
 from keras.regularizers import l1_l2
 
-def criarRede(params):
+def rede_cnnTrading(params):
     model = Sequential()
     model.add(Conv1D(filters=int(params['filters']), kernel_size=int(params['kernel_size']), activation=params['activation'], 
                      padding='same', kernel_initializer=params['kernel_initializer'], input_shape=(15, 1)))
     model.add(BatchNormalization())
     model.add(MaxPooling1D(pool_size=int(params['pool_size'])))
     model.add(Conv1D(filters=int(params['filters'] * 2), kernel_size=int(params['kernel_size']), activation=params['activation'], padding='same'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=int(params['pool_size'])))
+    model.add(Conv1D(filters=int(params['filters']), kernel_size=int(params['kernel_size']), activation=params['activation'], padding='same'))
     model.add(BatchNormalization())
     model.add(MaxPooling1D(pool_size=int(params['pool_size'])))
     model.add(Bidirectional(LSTM(units=int(params['lstm_units']), return_sequences=True)))
@@ -18,5 +22,5 @@ def criarRede(params):
     model.add(Dense(units=1, activation='tanh', kernel_initializer=params['kernel_initializer'], 
                     kernel_regularizer=l1_l2(l1=params['l1'], l2=params['l2'])))
 
-    model.compile(optimizer=params['optimizer'], loss=params['loss'], metrics=['accuracy'])
+    model.compile(optimizer=Adam(learning_rate=1e-4), loss=params['loss'], metrics=['accuracy'])
     return model
